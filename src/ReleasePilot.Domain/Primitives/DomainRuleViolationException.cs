6d8bdf9c24b82a5be39f8dfcc8d4e4943ebcd691
Jorge Gonzalo <jorge.gonzalo@outlook.com>
2026-03-17 17:37:50 +0100

    add Roslynator.Analyzers, xunit.analyzers and NSubstitute.Analyzers.CSharp static code analyser packages

--- src/ReleasePilot.Domain/Primitives/DomainRuleViolationException.cs
+++ src/ReleasePilot.Domain/Primitives/DomainRuleViolationException.cs
@@ -5,4 +5,12 @@ public sealed class DomainRuleViolationException : Exception
     public DomainRuleViolationException(string message) : base(message)
     {
     }
+
+    public DomainRuleViolationException()
+    {
+    }
+
+    public DomainRuleViolationException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
