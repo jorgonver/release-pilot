339842df048afdf9527b374599936f1a0b9ea3d1
Jorge Gonzalo <jorge.gonzalo@outlook.com>
2026-04-15 18:17:46 +0200

    extend api error helper with future responses

--- src/ReleasePilot.Api/Extensions/ControllerErrorResponseExtensions.cs
+++ src/ReleasePilot.Api/Extensions/ControllerErrorResponseExtensions.cs
@@ -5,8 +5,33 @@ namespace ReleasePilot.Api.Extensions;
 
 public static class ControllerErrorResponseExtensions
 {
+    public static BadRequestObjectResult BadRequestError(this ControllerBase controller, string message)
+    {
+        return controller.BadRequest(CreatePayload(controller, message));
+    }
+
+    public static ConflictObjectResult ConflictError(this ControllerBase controller, string message)
+    {
+        return controller.Conflict(CreatePayload(controller, message));
+    }
+
     public static NotFoundObjectResult NotFoundError(this ControllerBase controller, string message)
     {
-        return controller.NotFound(ApiErrorResponse.Create(message, controller.HttpContext.TraceIdentifier));
+        return controller.NotFound(CreatePayload(controller, message));
+    }
+
+    public static ObjectResult UnauthorizedError(this ControllerBase controller, string message)
+    {
+        return controller.StatusCode(StatusCodes.Status401Unauthorized, CreatePayload(controller, message));
+    }
+
+    public static ObjectResult ForbiddenError(this ControllerBase controller, string message)
+    {
+        return controller.StatusCode(StatusCodes.Status403Forbidden, CreatePayload(controller, message));
+    }
+
+    private static ApiErrorResponse CreatePayload(ControllerBase controller, string message)
+    {
+        return ApiErrorResponse.Create(message, controller.HttpContext.TraceIdentifier);
     }
 }
